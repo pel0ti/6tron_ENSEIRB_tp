802f9c1e256658497f74a8803a604f77428b1e52
EL KABBAJ Anass <Anass.El_Kabbaj@bordeaux-inp.fr>
2026-01-20 14:26:22 +0100

    added LED interrupt and timer features.

--- main.cpp
+++ main.cpp
@@ -4,14 +4,47 @@
  */
 #include "mbed.h"
 
-namespace {
-#define PERIOD_MS 2000ms
+using namespace std::chrono;
+
+DigitalOut   myled(LED1);
+InterruptIn  button(BUTTON1);
+Timer        t;
+
+volatile int flag = 0;
+
+constexpr auto PERIOD = 2000ms;
+
+void LEDset()
+{
+    myled = 1;
+    t.reset();
+    t.start();
+    flag = 1;
+}
+
+void LEDclr()
+{
+    myled = 0;
+    t.stop();
+    flag = 0;
 }
 
 int main()
 {
+    if (myled.is_connected()) {
+        printf(">> myled is initialized and connected!\n\r");
+    }
+
+    button.rise(&LEDset);
+    button.fall(&LEDclr);
+
     while (true) {
-        printf("Alive! \n");
-        ThisThread::sleep_for(PERIOD_MS / 2);
+        if (flag == 1) {
+            printf("[Timer ON] %lld us\n\r", t.elapsed_time().count());
+        } else {
+            t.reset();
+        }
+
+        ThisThread::sleep_for(PERIOD / 5);
     }
 }
