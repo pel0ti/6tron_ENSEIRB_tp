5cc43f353ab7ab1fed3213a716de6ca4cf5ca8f8
EL KABBAJ Anass <Anass.El_Kabbaj@bordeaux-inp.fr>
2026-01-20 15:07:24 +0100

    Added the ticker program

---
+++ timer.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2022, CATIE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include "mbed.h"
+
+using namespace std::chrono;
+
+DigitalOut   myled(LED1);
+InterruptIn  button(BUTTON1);
+Timer        t;
+
+volatile int flag = 0;
+
+constexpr auto PERIOD = 2000ms;
+
+void LEDset()
+{
+    myled = 1;
+    t.reset();
+    t.start();
+    //flag = 1;
+}
+
+void LEDclr()
+{
+    myled = 0;
+    t.stop();
+    flag = 1;
+}
+
+int main()
+{
+    if (myled.is_connected()) {
+        printf(">> myled is initialized and connected!\n\r");
+    }
+
+    button.rise(&LEDset);
+    button.fall(&LEDclr);
+
+    while (true) {
+        if (flag == 1) {
+            printf("[Timer ON] %lld us\n\r", t.elapsed_time().count());
+            flag = 0;
+        } else {
+            t.reset();
+        }
+
+        ThisThread::sleep_for(PERIOD / 5);
+    }
+}
